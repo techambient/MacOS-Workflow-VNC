46ed07aa0aa4f18dbd38b3dfdbe325cb42458596
techambient <tempu3544@gmail.com>
2026-09-16 12:37:54 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,33 +1,37 @@
-#configure.sh VNC_USER_PASSWORD VNC_PASSWORD NGROK_AUTH_TOKEN
+#!/bin/bash
 
-#disable spotlight indexing
+# Assign parameters to readable variables
+VNC_USER_PASSWORD="$1"
+VNC_PASSWORD="$2"
+NGROK_AUTH_TOKEN="$3"
+
+# Disable spotlight indexing to optimize performance
 sudo mdutil -i off -a
 
-#Create new account
+# Create new admin user account safely
 sudo dscl . -create /Users/vncuser
 sudo dscl . -create /Users/vncuser UserShell /bin/bash
 sudo dscl . -create /Users/vncuser RealName "VNC User"
 sudo dscl . -create /Users/vncuser UniqueID 1001
 sudo dscl . -create /Users/vncuser PrimaryGroupID 80
 sudo dscl . -create /Users/vncuser NFSHomeDirectory /Users/vncuser
-sudo dscl . -passwd /Users/vncuser $1
-sudo dscl . -passwd /Users/vncuser $1
+sudo dscl . -passwd /Users/vncuser "$VNC_USER_PASSWORD"
 sudo createhomedir -c -u vncuser > /dev/null
 
-#Enable VNC
+# Enable built-in Apple Remote Management / VNC server
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
 
-#VNC password - http://hints.macworld.com/article.php?story=20071103011608872
-echo $2 | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
+# Obfuscate and set the legacy VNC-only password string
+echo "$VNC_PASSWORD" | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
 
-#Start VNC/reset changes
+# Start and activate VNC service parameters
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
 
-#install ngrok
-brew cask install ngrok
+# Fixed: Install ngrok using modern Homebrew syntax
+brew install --cask ngrok
 
-#configure ngrok and start it
-ngrok authtoken $3
+# Authenticate ngrok and launch a background TCP tunnel on VNC port 5900
+ngrok config add-authtoken "$NGROK_AUTH_TOKEN"
 ngrok tcp 5900 &
