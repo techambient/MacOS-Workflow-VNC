c8aa7f03081c6306b29e486a28d184c811134250
techambient <tempu3544@gmail.com>
2026-09-16 12:59:21 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-
-# Assign parameters to readable variables
 VNC_USER_PASSWORD="$1"
 VNC_PASSWORD="$2"
 
-# Disable spotlight indexing to optimize performance
+# Opt-out of directory monitoring to bypass the permission error
+cd /tmp
+
+# Disable spotlight indexing
 sudo mdutil -i off -a
 
-# Create new admin user account safely
+# Create user safely in /tmp context
 sudo dscl . -create /Users/vncuser
 sudo dscl . -create /Users/vncuser UserShell /bin/bash
 sudo dscl . -create /Users/vncuser RealName "VNC User"
@@ -17,16 +18,16 @@ sudo dscl . -create /Users/vncuser NFSHomeDirectory /Users/vncuser
 sudo dscl . -passwd /Users/vncuser "$VNC_USER_PASSWORD"
 sudo createhomedir -c -u vncuser > /dev/null
 
-# Enable built-in Apple Remote Management / VNC server
+# Configure and turn on Screen Sharing
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
 
-# Obfuscate and set the legacy VNC-only password string
+# Inject VNC Password configuration profile safely
 echo "$VNC_PASSWORD" | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
 
-# Start and activate VNC service parameters
+# Reload service management agents
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
 
-# Start Pinggy TCP tunnel on VNC port 5900 (Prints the connection URL to the logs)
-ssh -p 443 -R0:localhost:5900 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null tcp.pinggy.io &
+# Open a secure bypass TCP tunnel using localhost.run (Prints out connection details)
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -R 5900:localhost:5900 nokey@localhost.run &
